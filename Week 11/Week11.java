2935e7c758e59ad04928547b7cf888139a3cb49c
Zahid Khan <xahiidkhan@gmail.com>
2020-06-24 10:21:33 +0530

    Added week 12

--- Week 11/Week11.java
+++ Week 11/Week11.java
@@ -2,28 +2,34 @@ import java.io.File;
 import java.lang.System.*;
 import java.io.*;
 import java.io.FileNotFoundException; // Import this class to handle errors 
-import java.util.Scanner; // Import the Scanner class to read text files
+import java.util.Scanner;
+import javax.xml.transform.*; // Import the Scanner class to read text files
 public class Week11{
 	public static void main(String str[]){
 	 try{
-		 File file1= new File("/sdcard/AppProjects/ShifaMaAm/src/Week 11/File1.txt");
-		 File file2= new File("/sdcard/AppProjects/ShifaMaAm/src/Week 11/File2.txt");
-		 File file3 = new File("/sdcard/AppProjects/ShifaMaAm/src/Week 11/File3.txt");
-		 File file4 = new File("/sdcard/AppProjects/ShifaMaAm/src/Week 11/File4.txt");
-		 //<<<<<CREATES FILE 3>>>>
+		String source ="/sdcard/AppProjects/ShifaMaAm/src/Week 11/";
+		 source = source + str[1];
+		 File Filesource = new File(source);
+		 
+		 String destination = "/sdcard/AppProjects/ShifaMaAm/src/Week 11/";
+		 destination = destination + str[2];
+		 File Filedestination = new File(destination);
+		 
+		 
+		 
 		 try {  
-			 if (!file3.createNewFile()) 
-				 System.out.println("File3 already exists."); ; 
+			 if (!Filedestination.createNewFile()) 
+				 System.out.println("Destination already exists."); 
 		 } //try
 		 catch (IOException e) { 
-			 System.out.println("An error occurred."); 
+			 System.out.println("An error occurred on destination file."); 
 			 e.printStackTrace();
 		 }//catch
 
 		 //<<<<<CREATES FILE 4>>>>
 		 try {  
-			 if (!file4.createNewFile()) 
-				 System.out.println("File4 already exists."); ; 
+			 if (!Filesource.createNewFile()) 
+				 System.out.println("Source already exists.");
 		 } //try
 		 catch (IOException e) { 
 			 System.out.println("An error occurred."); 
@@ -31,64 +37,29 @@ public class Week11{
 		 }//catch
 		 
 		 
-		 Scanner readF1 = new Scanner(file1);
-		 Scanner readF2 = new Scanner(file2);
+		 Scanner readSource = new Scanner(Filesource);
+		// Scanner readDestination = new Scanner(destination);
 		//W(3) <= R(2);
 		 try{ 				
-		 FileWriter writeF3 = new FileWriter("/sdcard/AppProjects/ShifaMaAm/src/Week 11/File3.txt");
-		 while(readF2.hasNextLine()){
-			String data = readF2.nextLine();
+		 FileWriter writeDestination = new FileWriter(destination);
+		 while(readSource.hasNextLine()){
+			String data = readSource.nextLine();
 			data = data.concat("\n");
 			System.out.println(data);
-			writeF3.write(data);
+			writeDestination.write(data);
 		 }
-		 writeF3.close();
+		 writeDestination.close();
 		}catch(IOException e){
 			System.out.println("Error");
 		} //catch
 		
+		 
+		 readSource.close();
 
-		
-		//W(4)<== R(1)
-		 try{				//writes file4.
-			 FileWriter writeF4 = new FileWriter("/sdcard/AppProjects/ShifaMaAm/src/Week 11/File4.txt");
-			 while(readF1.hasNextLine()){
-				 String data = readF1.nextLine();
-				 data = data.concat("\n");
-				 System.out.println(data);
-				 writeF4.write(data);
-			 }
-			 writeF4.close();
-		 }catch(IOException e){
-			 System.out.println("Error");
-		 } //catch
-		 
-		
-		 
-		 readF1.close();
-		 readF2.close();
-		 
-		 if (file1.delete()) { 
-			 System.out.println("Replaced content of : " + file1.getName());
-		 } else { 
-		 	System.out.println("Failed to delete the file."); 
-		 } 
-		
-		 
-		 if (file2.delete()) { 
-			 System.out.println("Replaced content of: " + file2.getName());
-		 } else { 
-			 System.out.println("Failed to delete the file."); 
-		 }
-		 
-		 file3.renameTo(file1);
-		 file4.renameTo(file2);
-	}//Try.
-		catch(FileNotFoundException e){
-		System.out.println("An error occurred.");
-		e.printStackTrace();
-	   }
-	  
+	}
+	catch(FileNotFoundException e){
+		System.out.println(e);
+	}  
 	}//Void main
 }//class.
 
